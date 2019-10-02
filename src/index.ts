36bc56c9daf8b0a7e1f12ae852b69df0b5d2d1a8
shybyte <ein@volloeko.de>
2019-10-02 06:57:09 +0200

    Added codemirror

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hossa');
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/addon/display/placeholder.js';
+
+const codeMirror = CodeMirror(document.getElementById('codemirrorContainer')!, {
+  lineNumbers: true,
+  placeholder: 'Type here!'
+});
+
+codeMirror.focus();
