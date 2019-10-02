add702880f50d037b1b1af0e5a67f7c70952158f
shybyte <ein@volloeko.de>
2019-10-02 07:12:19 +0200

    Added sidebar

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as acrolinxSdk from '@acrolinx/sidebar-sdk';
 import CodeMirror from 'codemirror';
 import 'codemirror/mode/javascript/javascript.js';
 import 'codemirror/mode/htmlmixed/htmlmixed';
@@ -5,7 +6,26 @@ import 'codemirror/addon/display/placeholder.js';
 
 const codeMirror = CodeMirror(document.getElementById('codemirrorContainer')!, {
   lineNumbers: true,
-  placeholder: 'Type here!'
+  placeholder: 'Type here!',
+  extraKeys: {Tab: false}
 });
 
 codeMirror.focus();
+
+const CLIENT_SIGNATURE = 'SW50ZWdyYXRpb25EZXZlbG9wbWVudERlbW9Pbmx5';
+
+const acrolinxPlugin = new acrolinxSdk.AcrolinxPlugin({
+  sidebarContainerId: 'sidebarContainer',
+  clientSignature: CLIENT_SIGNATURE,
+  clientComponents: [
+    {
+      id: 'wriber',
+      name: 'Wriber',
+      version: '1.2.3.4',
+      category: 'MAIN'
+    }
+  ]
+});
+
+acrolinxPlugin.registerAdapter(new acrolinxSdk.CodeMirrorAdapter({editor: codeMirror}));
+acrolinxPlugin.init();
